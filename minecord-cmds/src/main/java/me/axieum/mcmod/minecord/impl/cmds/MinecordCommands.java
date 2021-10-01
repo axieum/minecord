8e143e457eeafc02f430ea1bfbb47b86b219c61f
axieum <7577851+axieum@users.noreply.github.com>
2021-10-01 21:48:00 +1000

    feat(cmds): use minecord addon entrypoint

--- minecord-cmds/src/main/java/me/axieum/mcmod/minecord/impl/cmds/MinecordCommands.java
+++ minecord-cmds/src/main/java/me/axieum/mcmod/minecord/impl/cmds/MinecordCommands.java
@@ -1,20 +1,20 @@
 package me.axieum.mcmod.minecord.impl.cmds;
 
 import me.shedaniel.autoconfig.ConfigHolder;
+import net.dv8tion.jda.api.JDABuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
-
+import me.axieum.mcmod.minecord.api.addon.MinecordAddon;
 import me.axieum.mcmod.minecord.impl.cmds.config.CommandConfig;
 
-public final class MinecordCommands implements PreLaunchEntrypoint
+public final class MinecordCommands implements MinecordAddon
 {
     public static final Logger LOGGER = LogManager.getLogger("Minecord|Commands");
     private static final ConfigHolder<CommandConfig> CONFIG = CommandConfig.init();
 
     @Override
-    public void onPreLaunch()
+    public void onInitializeMinecord(JDABuilder builder)
     {
         LOGGER.info("Minecord Commands is getting ready...");
     }
