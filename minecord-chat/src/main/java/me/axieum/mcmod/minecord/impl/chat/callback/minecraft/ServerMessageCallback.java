e746ef6718f3d83093ff6c0a200602ec7405b46d
jonathan <7577851+axieum@users.noreply.github.com>
2022-08-08 19:12:20 +1000

    feat(chat): use new `fabric-message-api` events (closes #32)

--- minecord-chat/src/main/java/me/axieum/mcmod/minecord/impl/chat/callback/minecraft/ServerMessageCallback.java
+++ minecord-chat/src/main/java/me/axieum/mcmod/minecord/impl/chat/callback/minecraft/ServerMessageCallback.java
@@ -1,21 +1,27 @@
 package me.axieum.mcmod.minecord.impl.chat.callback.minecraft;
 
+import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.filter.FilteredMessage;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.registry.RegistryKey;
+
+import net.fabricmc.fabric.api.message.v1.ServerMessageEvents.ChatMessage;
 
 import me.axieum.mcmod.minecord.api.Minecord;
 import me.axieum.mcmod.minecord.api.chat.event.ChatPlaceholderEvents;
-import me.axieum.mcmod.minecord.api.chat.event.minecraft.ReceiveChatCallback;
 import me.axieum.mcmod.minecord.api.util.StringTemplate;
 import me.axieum.mcmod.minecord.api.util.StringUtils;
 import me.axieum.mcmod.minecord.impl.chat.util.DiscordDispatcher;
 
-public class PlayerChatCallback implements ReceiveChatCallback
+public class ServerMessageCallback implements ChatMessage
 {
     @Override
-    public void onReceiveChat(ServerPlayerEntity player, FilteredMessage<SignedMessage> message)
+    public void onChatMessage(
+        FilteredMessage<SignedMessage> message, ServerPlayerEntity player, RegistryKey<MessageType> typeKey
+    )
     {
+        if (!MessageType.CHAT.equals(typeKey)) return;
         Minecord.getInstance().getJDA().ifPresent(jda -> {
             /*
              * Prepare a message template.
@@ -34,14 +40,14 @@ public class PlayerChatCallback implements ReceiveChatCallback
                 message.filteredOrElse(message.raw()).getContent().getString()
             ));
 
-            ChatPlaceholderEvents.Minecraft.PLAYER_CHAT.invoker().onPlayerChatPlaceholder(st, player, message);
+            ChatPlaceholderEvents.Minecraft.PLAYER_CHAT.invoker().onPlayerChatPlaceholder(st, player, message, typeKey);
 
             /*
              * Dispatch the message.
              */
 
-            DiscordDispatcher.dispatch((builder, entry) ->
-                    builder.setContent(st.format(entry.discord.chat)),
+            DiscordDispatcher.dispatch((embed, entry) ->
+                    embed.setContent(st.format(entry.discord.chat)),
                 entry -> entry.discord.chat != null && entry.hasWorld(player.world));
         });
     }
